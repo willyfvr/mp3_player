8001a30eb64c2620e48bcf16737e94402ef99763
Victor Fattor <victor.fattor@bosonic.digital>
2026-01-09 22:54:30 -0300

    add app structure and home screen

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import '../screens/home_screen.dart';
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tiburoncin MP3',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
